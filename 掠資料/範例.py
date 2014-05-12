ab04ca227dd401cad051d3e78c4181ed16a5de3f
Ihc <ihcaoe@gmail.com>
2014-05-12 16:51:44 +0800

    會使振動 丞宏

---
+++ 掠資料/範例.py
@@ -0,0 +1,32 @@
+import urllib.parse
+from 掠資料.GoogleScrape import scrape
+'''
+http://incolumitas.com/2013/01/06/googlesearch-a-rapid-python-class-to-get-search-results/
+sudo pip3.4  install lxml
+sudo pip3.4  install requests
+sudo pip3.4  install cssselect
+sudo pip3.4  install BeautifulSoup4
+sudo pip3.4  install SocksiPy-branch
+'''
+
+if __name__ == '__main__':
+
+    results = scrape('Best SEO tool', num_results_per_page=50, num_pages=3, offset=0)
+    for page in results:
+        for link_title, link_snippet, link_url in page['results']:
+            # You can access all parts of the search results like that
+            # link_url.scheme => URL scheme specifier (Ex: 'http')
+            # link_url.netloc => Network location part (Ex: 'www.python.org')
+            # link_url.path => URL scheme specifier (Ex: ''help/Python.html'')
+            # link_url.params => Parameters for last path element
+            # link_url.query => Query component
+            try:
+                print(urllib.parse.unquote(link_url.geturl())) # This reassembles the parts of the url to the whole thing
+            except:
+                pass
+
+# How many urls did we get on all pages?
+print(sum(len(page['results']) for page in results))
+
+# How many hits has google found with our keyword (as shown on the first page)?
+print(results[0]['num_results_for_kw'])
