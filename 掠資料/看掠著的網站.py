c0a621a9d396c5dfdc2eddac39f59d8a4d4d0d46
chhsueh <ihcaod@gmail.com>
2014-05-13 17:35:14 +0800

    共預設的格式提掉 丞宏

--- 掠資料/看掠著的網站.py
+++ 掠資料/看掠著的網站.py
@@ -28,16 +28,21 @@ class 看掠著的網站:
 					資料.append(self.對檔案看(所在 + '/' + 檔名))
 		return '\n\n'.join(資料)
 	def 提網頁文字出來(self, 網頁html):
-		return html2text.html2text(網頁html).strip()
+		h = html2text.HTML2Text()
+		h.ignore_links = True
+		h.ignore_emphasis=True
+		h.ignore_images=True
+		h.google_doc=False
+		return h.handle(網頁html).strip().replace('\\-','-')
 
 if __name__ == '__main__':
 # 	print(看掠著的網站().對網頁看("http://taigu.fhl.net/index.html"))
 # 	print(看掠著的網站().對資料夾看('../網頁資料'))
 	print(看掠著的網站().提網頁文字出來(
-	"""
+	"""<p>Hello, <a href='http://earth.google.com/'>world</a>!
 	<p style='layout-grid-mode: char; margin-top:5px; margin-bottom:5px'>
-<font size="3"><font face="Taigi Unicode">旭</font><span lang="EN-NZ" style="font-family: Taigi Unicode; ">--á 
-kap</span><font face="Taigi Unicode">董</font><span lang="EN-NZ" style="font-family: Taigi Unicode; ">--ê</span></font><font FACE="新細明體" LANG="ZH-TW"><span lang="EN-US" style="font-family: Taigi Unicode"><font size="3"> 
+<font size="3"><font face="Taigi Unicode">XD</font>---á --a--<b>---</b> --a--<b>---</b> 
+kap<font face="Taigi Unicode">@@</font><span lang="EN-NZ" style="font-family: Taigi Unicode; ">--ê</span></font><font FACE="新細明體" LANG="ZH-TW"><span lang="EN-US" style="font-family: Taigi Unicode"><font size="3"> 
 / Ui-chì</font></span></font><span lang="EN-US" style="font-family: Taiwanese Serif"><FONT FACE="Taigi Unicode" LANG="ZH-TW"><o:p><font size="3">
 </font>
       </o:p>
