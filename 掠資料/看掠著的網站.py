025d724d4b92ee47af18588626effe33202ceec8
chhsueh <ihcaod@gmail.com>
2014-05-13 16:22:06 +0800

    補讀檔案 丞宏

--- 掠資料/看掠著的網站.py
+++ 掠資料/看掠著的網站.py
@@ -1,14 +1,15 @@
 from urllib.request import urlopen
-import lxml.html
-import lxml.html.clean
 import html2text
 
 class 看掠著的網站:
-	def 對網頁提(self,網址):
-		with urlopen(網址) as data:
-			return self.提網頁文字出來(data.read().decode('utf-8'))
+	def 對網頁看(self,網址):
+		with urlopen(網址) as 資料:
+			return self.提網頁文字出來(資料.read().decode('utf-8'))
+	def 對檔案看(self,檔名):
+		with open(檔名) as 資料:
+			return self.提網頁文字出來(資料.read())
 	def 提網頁文字出來(self,網頁html):
 		return html2text.html2text(網頁html)
 
 if __name__=='__main__':
-	看掠著的網站().對網頁提("http://taigu.fhl.net/index.html")
+	print(看掠著的網站().對網頁看("http://taigu.fhl.net/index.html"))
