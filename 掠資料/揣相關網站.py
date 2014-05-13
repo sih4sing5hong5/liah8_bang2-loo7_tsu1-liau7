3f6c9d0997a02a9d9629fbae465d4c9cda6fd679
chhsueh <ihcaod@gmail.com>
2014-05-13 16:30:00 +0800

    定好傳回來的格式 丞宏

--- 掠資料/揣相關網站.py
+++ 掠資料/揣相關網站.py
@@ -2,20 +2,20 @@ import urllib
 from 掠資料.GoogleScrape import scrape
 
 class 揣相關網站:
-	def 揣(self,關鍵字):
-		
-		results = scrape(關鍵字, num_results_per_page=50, num_pages=3, offset=0)
+	def 揣(self, 關鍵字, 幾筆=500):
+		results = scrape(關鍵字, num_results_per_page=50, num_pages=(幾筆 - 1) // 50 + 1, offset=0)
+		結果 = []
 		for page in results:
 			for link_title, link_snippet, link_url in page['results']:
-				# You can access all parts of the search results like that
-				# link_url.scheme => URL scheme specifier (Ex: 'http')
-				# link_url.netloc => Network location part (Ex: 'www.python.org')
-				# link_url.path => URL scheme specifier (Ex: ''help/Python.html'')
-				# link_url.params => Parameters for last path element
-				# link_url.query => Query component
 				try:
-					print(urllib.parse.unquote(link_url.geturl())) # This reassembles the parts of the url to the whole thing
+					結果.append(
+						(link_title,
+						urllib.parse.unquote(link_url.geturl()),
+						link_snippet,)
+					)
 				except:
 					pass
-				# How many hits has google found with our keyword (as shown on the first page)?
-		print(results[0]['num_results_for_kw'])
+		return 結果
+if __name__ == '__main__':
+	結果 = 揣相關網站().揣('無法度')
+	print(結果)
